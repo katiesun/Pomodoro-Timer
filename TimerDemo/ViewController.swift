6fcb9ffc71adee52eb4e19c41869d6d0a1c93e9d
Katherine Sun <katie.sun22@gmail.com>
2020-02-07 09:47:22 -0600

    Initial Commit

---
+++ TimerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimerDemo
+//
+//  Created by Katherine Sun on 2/7/20.
+//  Copyright © 2020 Katherine Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
